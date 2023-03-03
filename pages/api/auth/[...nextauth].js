4a91b9317a3aeccf5e2932e10d2b027d4a0815f7
Muhammad Naufal <muhamadnaufal429@gmail.com>
2023-03-03 09:05:07 +0700

    fix google provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,6 +9,7 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      secret: process.env.SECRET,
     }),
   ],
 };
